07d7149e0e733b3a45a794e92030c9fc56ba7af7
Daniel J. R. May <daniel.may@danieljrmay.com>
2018-12-05 12:26:13 +0000

    Remove unnecessary whitespace

--- src/DateTimeFormatter.js
+++ src/DateTimeFormatter.js
@@ -56,114 +56,114 @@ function dateToString(format, date = new Date()) {
     let output = '';
     
     while (i < LENGTH) {
-	ch = format.charAt(i);
-	
-	if (ch == '%' && ++i < LENGTH) {
-	    switch (format.charAt(i)) {
-	    case 'A':
-		output += _getWeekdayLong(date.getDay());
-		break;
-	    case 'B':
-		output += _getMonthLong(date.getMonth());
+        ch = format.charAt(i);
+        
+        if (ch == '%' && ++i < LENGTH) {
+            switch (format.charAt(i)) {
+            case 'A':
+                output += _getWeekdayLong(date.getDay());
                 break;
-	    case 'D':
-		output += date.getMonth().toString().padStart(2, '0')
-		    + '/' + date.getDate().toString().padStart(2, '0')
-		    + '/' + date.getFullYear().toString().slice(-2);
-		break;
-	    case 'F':
-		output += date.getFullYear()
-		    + '-' + date.getMonth().toString().padStart(2, '0')
-		    + '-' + date.getDate().toString().padStart(2, '0');
-		break;
-	    case 'H':
-		output += date.getHours().toString().padStart(2, '0');
-		break;
-	    case 'I':
-		output += _get12Hour(date.getHours()).toString().padStart(2, '0');
-		break;
-	    case 'M':
-		output += date.getMinutes().toString().padStart(2, '0');
-		break;
-	    case 'P':
-		output =+ (date.getHours() < 12) ? 'am' : 'pm';
-		break;
-	    case 'R':
-		output += date.getHours().toString().padStart(2, '0')
-		    + ':' + date.getMinutes().toString().padStart(2, '0');
-		break;
-	    case 'S':
-		output += date.getSeconds().toString().padStart(2, '0');
-		break;
-	    case 'T':
-		output += date.getHours().toString().padStart(2, '0')
-		    + ':' + date.getMinutes().toString().padStart(2, '0') 
-		    + ':' + date.getSeconds().toString().padStart(2, '0');
-		break;
-	    case 'X':
-		output += date.toLocaleTimeString();
-		break;
-	    case 'Y':
-		output += date.getFullYear();
-		break;
-	    case 'a':
-		output += _getWeekdayShort(date.getDay())
-		break;
-	    case 'b':
-		output += _getMonthShort(date.getMonth());
+            case 'B':
+                output += _getMonthLong(date.getMonth());
                 break;
-	    case 'c':
-		output += date.toLocaleString();
+            case 'D':
+                output += date.getMonth().toString().padStart(2, '0')
+                    + '/' + date.getDate().toString().padStart(2, '0')
+                    + '/' + date.getFullYear().toString().slice(-2);
                 break;
-	    case 'd':
-		output += date.getDate().toString().padStart(2, '0');
-		break;
-	    case 'e':
-		output += date.getDate().toString().padStart(2, ' ');
-		break;
-	    case 'f':
-		output += date.getDate();
-		break;
-	    case 'k':
-		output += date.getHours();
-		break;
-	    case 'l':
-		output += _get12Hour(date.getHours())
-		break;
-	    case 'm':
-		output += date.getMonth().toString().padStart(2, '0');
-		break;
-	    case 'p':
-		output =+ (date.getHours() < 12) ? 'AM' : 'PM';
-		break;
-	    case 'r':
-		const MERIDIEM = (date.getHours() < 12) ? 'AM' : 'PM'
-		output += _get12Hour(date.getHours()).toString().padStart(2, '0')
-		    + ':' + date.getMinutes().toString().padStart(2, '0')
-		    + ':' + date.getSeconds().toString().padStart(2, '0')
-		    + ' ' + MERIDIEM;
-		break;
-	    case 's':
-		output += date.getTime();
-		break;
-	    case 'w':
-		output += date.getDay();
-		break;
-	    case 'x':
-		output += date.toLocaleDateString();
-		break;
-	    case 'y':
-		output += date.getFullYear().toString().slice(-2);
-		break;
-	    default:
-		output += '%' + format.charAt(i);
-		break;
-	    }
-	} else {
-	    output += ch;
-	}
+            case 'F':
+                output += date.getFullYear()
+                    + '-' + date.getMonth().toString().padStart(2, '0')
+                    + '-' + date.getDate().toString().padStart(2, '0');
+                break;
+            case 'H':
+                output += date.getHours().toString().padStart(2, '0');
+                break;
+            case 'I':
+                output += _get12Hour(date.getHours()).toString().padStart(2, '0');
+                break;
+            case 'M':
+                output += date.getMinutes().toString().padStart(2, '0');
+                break;
+            case 'P':
+                output =+ (date.getHours() < 12) ? 'am' : 'pm';
+                break;
+            case 'R':
+                output += date.getHours().toString().padStart(2, '0')
+                    + ':' + date.getMinutes().toString().padStart(2, '0');
+                break;
+            case 'S':
+                output += date.getSeconds().toString().padStart(2, '0');
+                break;
+            case 'T':
+                output += date.getHours().toString().padStart(2, '0')
+                    + ':' + date.getMinutes().toString().padStart(2, '0') 
+                    + ':' + date.getSeconds().toString().padStart(2, '0');
+                break;
+            case 'X':
+                output += date.toLocaleTimeString();
+                break;
+            case 'Y':
+                output += date.getFullYear();
+                break;
+            case 'a':
+                output += _getWeekdayShort(date.getDay())
+                break;
+            case 'b':
+                output += _getMonthShort(date.getMonth());
+                break;
+            case 'c':
+                output += date.toLocaleString();
+                break;
+            case 'd':
+                output += date.getDate().toString().padStart(2, '0');
+                break;
+            case 'e':
+                output += date.getDate().toString().padStart(2, ' ');
+                break;
+            case 'f':
+                output += date.getDate();
+                break;
+            case 'k':
+                output += date.getHours();
+                break;
+            case 'l':
+                output += _get12Hour(date.getHours())
+                break;
+            case 'm':
+                output += date.getMonth().toString().padStart(2, '0');
+                break;
+            case 'p':
+                output =+ (date.getHours() < 12) ? 'AM' : 'PM';
+                break;
+            case 'r':
+                const MERIDIEM = (date.getHours() < 12) ? 'AM' : 'PM'
+                output += _get12Hour(date.getHours()).toString().padStart(2, '0')
+                    + ':' + date.getMinutes().toString().padStart(2, '0')
+                    + ':' + date.getSeconds().toString().padStart(2, '0')
+                    + ' ' + MERIDIEM;
+                break;
+            case 's':
+                output += date.getTime();
+                break;
+            case 'w':
+                output += date.getDay();
+                break;
+            case 'x':
+                output += date.toLocaleDateString();
+                break;
+            case 'y':
+                output += date.getFullYear().toString().slice(-2);
+                break;
+            default:
+                output += '%' + format.charAt(i);
+                break;
+            }
+        } else {
+            output += ch;
+        }
 
-	i++;
+        i++;
     }
 
     return output;
@@ -179,28 +179,28 @@ function dateToString(format, date = new Date()) {
 function _getWeekdayLong(dayAsInt) {
     switch(dayAsInt) {
     case 0:
-	return 'Sunday';
+        return 'Sunday';
 
     case 1:
-	return 'Monday';
+        return 'Monday';
 
     case 2:
-	return 'Tuesday';
+        return 'Tuesday';
 
     case 3:
-	return 'Wednesday';
+        return 'Wednesday';
 
     case 4:
-	return 'Thursday';
+        return 'Thursday';
 
     case 5:
-	return 'Friday';
+        return 'Friday';
 
     case 6:
-	return 'Saturday';
+        return 'Saturday';
 
     default:
-	throw 'Illegal value for dayAsInt.' 
+        throw 'Illegal value for dayAsInt.' 
     }
 }
 
@@ -214,28 +214,28 @@ function _getWeekdayLong(dayAsInt) {
 function _getWeekdayShort(dayAsInt) {
     switch(dayAsInt) {
     case 0:
-	return 'Sun';
+        return 'Sun';
 
     case 1:
-	return 'Mon';
+        return 'Mon';
 
     case 2:
-	return 'Tue';
+        return 'Tue';
 
     case 3:
-	return 'Wed';
+        return 'Wed';
 
     case 4:
-	return 'Thu';
+        return 'Thu';
 
     case 5:
-	return 'Fri';
+        return 'Fri';
 
     case 6:
-	return 'Sat';
+        return 'Sat';
 
     default:
-	throw 'Illegal value for dayAsInt.' 
+        throw 'Illegal value for dayAsInt.' 
     }
 }
 
@@ -249,7 +249,7 @@ function _getWeekdayShort(dayAsInt) {
 function _getMonthLong(monthAsInt) {
     switch(monthAsInt) {
     case 0:
-	return 'January';
+        return 'January';
         
     case 1:
         return 'February';
@@ -285,7 +285,7 @@ function _getMonthLong(monthAsInt) {
         return 'December';
 
     default:
-	throw 'Illegal value for monthAsInt.';
+        throw 'Illegal value for monthAsInt.';
     }
 }
 
@@ -299,7 +299,7 @@ function _getMonthLong(monthAsInt) {
 function _getMonthShort(monthAsInt) {
     switch(monthAsInt) {
     case 0:
-	return 'Jan';
+        return 'Jan';
         
     case 1:
         return 'Feb';
@@ -335,7 +335,7 @@ function _getMonthShort(monthAsInt) {
         return 'Dec';
 
     default:
-	throw 'Illegal value for monthAsInt.';
+        throw 'Illegal value for monthAsInt.';
     }
 }
 
@@ -348,11 +348,11 @@ function _getMonthShort(monthAsInt) {
 ///
 function _get12Hour(hourAsInt) {
     if (hourAsInt > 12) {
-	return hourAsInt - 12;
+        return hourAsInt - 12;
     }
 
     if (hourAsInt == 0) {
-	return 12;
+        return 12;
     }
 
     return hourAsInt;
